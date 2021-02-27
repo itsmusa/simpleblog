d5f2d15042e116dba898e918074ed090b9ce99db
Musa Magwaza <65224007+itsmusa@users.noreply.github.com>
2021-02-27 21:46:44 +0200

    heroku

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 MEDIA_URL = '/simpleblog/'  # or any prefix you choose
